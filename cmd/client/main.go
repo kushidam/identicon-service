4b98dd3ebaa27987f3ddf93a5ada36e52d35862e
kushidam <masaki.kushida.var@gmail.com>
2023-11-09 20:10:56 +0900

    [Add] Client gPRC Connect

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
+
+	"connectrpc.com/connect"
+	identiconv1 "github.com/kushidam/identicon-service/gen/identicon/v1"
+	identiconv1connect "github.com/kushidam/identicon-service/gen/identicon/v1/identiconv1connect"
 )
 
 
@@ -13,5 +20,24 @@ var (
 func main(){
 	flag.Parse()
 	fmt.Println(userText)
+
+	// gRPCクライアントを作成
+	client := identiconv1connect.NewIdenticonServiceClient(
+		http.DefaultClient,
+		"http://localhost:50051",
+	)
+
+
+	// GenerateIdenticonメソッドを呼び出す
+	res, err := client.GenerateIdenticon(
+        context.Background(),
+        connect.NewRequest(&identiconv1.GenerateIdenticonRequest{Text: *userText}),
+    )
+
+	if err != nil {
+		log.Fatalf("GenerateIdenticonの呼び出しエラー: %v", err)
+	}
 	
+	log.Printf("生成されたアイデンティコンのバイトデータ: %v", res.Msg.ImageData)
+
 }
